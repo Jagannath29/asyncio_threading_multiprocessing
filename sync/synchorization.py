3469590407716a2ccf4a754739795762f79391f3
Jagannath29 <khadkaj84@gmail.com>
2024-03-22 07:58:51 +0545

    Synchorization

---
+++ sync/synchorization.py
@@ -0,0 +1,14 @@
+def doFirst():
+    print('First running....')
+
+def doSecond():
+    print("Second running....")
+
+
+def main():
+    doFirst()
+    doSecond()
+
+
+if __name__ == '__main__':
+    main()
